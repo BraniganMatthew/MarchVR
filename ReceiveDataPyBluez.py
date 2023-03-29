a5b00f5ab5c2643b801d51e67f14aa241c7355f7
jibinalex <jibincalex@gmail.com>
2023-03-29 17:24:52 -0400

    Merge with main

--- ReceiveDataPyBluez.py
+++ ReceiveDataPyBluez.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-# Jibin Alex
-# Test program that is supposed to receive data through Bluetooth
-# Follow the steps to download the PyBluez library and all of its dependencies prior to running the program
-
-import bluetooth
-
-ESP32MACaddress = "E8:9F:6D:26:9F:1A"
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((ESP32MACaddress, 1))
-
-try:
-    while True:
-        data = s.recv(1)
-        if (data):
-            print(data)
-
-except KeyboardInterrupt:
-    print("Execution interrupted")
-
-# https://blog.kevindoran.co/bluetooth-programming-with-python-3/
-
-# pip install git+https://github.com/pybluez/pybluez.git#egg=pybluez
-=======
 # Jibin Alex (Modified by Matthew Branigan)(Modified by Maria Carmona)
 # Test program that is supposed to receive data through Bluetooth
 # Follow the steps to download the PyBluez library and all of its dependencies prior to running the program
@@ -165,4 +141,3 @@ except KeyboardInterrupt:
     print("Execution interrupted")
 
 # https://blog.kevindoran.co/bluetooth-programming-with-python-3/
->>>>>>> 65812e1c4c27670725b7446812937a509d70af27
