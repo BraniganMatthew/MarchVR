8f380df029a53aa38734ad44fef91084ed906a6c
PatrickHJR <harrisp@ufl.edu>
2023-09-05 13:31:12 -0400

    updated the ui to have graph placeholders

--- test-march-ui.py
+++ test-march-ui.py
@@ -1,6 +1,11 @@
 from PySide6.QtWidgets import *
 from PySide6.QtGui import QKeySequence, QPalette, QColor, QAction, QFont
 from PySide6.QtCore import QtMsgType
+from PySide6 import QtCore, QtGui, QtWidgets
+import pyqtgraph
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from BlurWindow.blurWindow import blur
 import socket
 import sys
@@ -25,11 +30,31 @@ buffer_size = 1024
 
 winWidth = 960
 winHeight = 540
-winX = 100
-winY = 100
+winX = 280
+winY = 160
 
 green = QColor(0, 200, 20)
 red = QColor(200, 5, 5)
+
+class Canvas(FigureCanvas):
+    def __init__(self, parent, title, x, y, w, h):
+        fig, self.ax = plt.subplots(figsize = (3,3), dpi=100)
+        super().__init__(fig)
+        self.setParent(parent)
+        self.ax.set_axis_off()
+
+        """ 
+        Matplotlib Script
+        """
+        t = np.arange(0.0, 2.0, 0.01)
+        s = 1 + np.sin(2 * np.pi * t)
+        
+        self.ax.plot(t, s)
+
+        self.ax.set(xlabel='time (s)', ylabel='voltage (mV)')
+        self.ax.set_title(title, fontsize = 10)
+        self.ax.grid()
+        self.setGeometry(x, y, w, h)
 
 class Window(QMainWindow):
     def __init__(self):
@@ -45,6 +70,7 @@ class Window(QMainWindow):
   
         # setting geometry
         self.setGeometry(winX, winY, winWidth, winHeight)
+        self.setFixedSize(winWidth, winHeight)
   
         # calling method
         self.UiComponents()
@@ -94,6 +120,7 @@ class Window(QMainWindow):
         mainText = QLabel("March VR Hub", self)
         mainText.setGeometry(mainX, mainY, 200, 50)
         mainText.setStyleSheet("QLabel{font-size: 18pt;}")
+        mainText.setAlignment(QtCore.Qt.AlignCenter)
 
         # TRACKER 1 TEXT -------------------------------------------------------------------------------
         t1x = 25
@@ -108,6 +135,7 @@ class Window(QMainWindow):
         t1con.setStyleSheet("QLabel{font-size: 12pt; color: green}")
 
         # not connected text
+        global t1discon
         t1discon = QLabel("Disonnected", self)
         t1discon.setGeometry(t1x + 70, t1y, 200, 50)
         t1discon.setStyleSheet("QLabel{font-size: 12pt color: red;}")
@@ -130,6 +158,11 @@ class Window(QMainWindow):
         t2discon.setGeometry(t2x + 70, t2y, 200, 50)
         t2discon.setStyleSheet("QLabel{font-size: 12pt; color: red}")
         t2discon.hide()
+
+        chart1a = Canvas(self, "Tracker 1 Accelerometer", 400, 50, 200, 200)
+        chart2a = Canvas(self, "Tracker 2 Accelerometer", 650, 50, 200, 200)
+        chart1g = Canvas(self, "Tracker 1 Gyroscope", 400, 300, 200, 200)
+        chart2g = Canvas(self, "Tracker 2 Gyroscope", 650, 300, 200, 200)
   
     
     # action method
@@ -142,15 +175,19 @@ class Window(QMainWindow):
         print("Recalibrate pressed")
 
     def clickCon(self):
-        print("Connect pressed")
+        print("Connect pressed")   
 
+#App = QApplication(sys.argv)        
     
   
 # create pyqt5 app
 App = QApplication(sys.argv)
+
+# demo = AppDemo()
+# demo.show()
   
 # create the instance of our Window
 window = Window()
-  
+
 # start the app
 sys.exit(App.exec())
